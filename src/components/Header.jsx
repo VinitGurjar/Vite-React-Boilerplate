abc059178f52e22380ba5f2383f54e16a56e8b94
adhiramaa <vinitfagna@gmail.com>
2023-03-22 23:56:21 +0530

    Boilerplate code template which is in production is uploaded

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+//Write rafce + tab to generate the boilerplate code.
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>{ import.meta.env.VITE_API_URL }</div>
+  )
+}
+
+export default Header
